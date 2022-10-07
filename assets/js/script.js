634fa83a897bcf5ce8c3520f7e85ec4ed390cf0b
muhammad.ananto <muhammad.ananto@students.amikom.ac.id>
2022-10-07 13:41:48 +0700

    update upload new data

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+
+let submit = document.querySelector('.add-item');
+
+let sImg = document.querySelector('#sImg');
+let sTitle = document.querySelector('#sTitle');
+let sPrice = document.querySelector('#sPrice');
+
+submit.addEventListener('click', function(){
+    let pName = document.querySelector('input[name="pName"]').value;
+    let pPrice = document.querySelector('input[name="pPrice"]').value;
+
+    sTitle.innerHTML=pName;
+    sPrice.innerHTML=pPrice;
+
+
+    let pPhoto = document.querySelector('input[name="pPhoto"]');
+
+    const file = pPhoto.files[0];
+
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = function (){
+        sImg.setAttribute('src', fileReader.result);
+        // images[0].setAttribute('style', `background-image: url('${fileReader.result}')`);
+    }
+
+    alert('success upload data');
+    pName.value=''
+    pPrice.value=''
+    pPhoto.files=''
+})
