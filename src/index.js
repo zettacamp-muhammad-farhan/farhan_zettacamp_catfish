4dd2d486c98473e793e24e99a93ef8d6a023f17d
muhammad.ananto <muhammad.ananto@students.amikom.ac.id>
2022-10-14 19:17:01 +0700

    new

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-var word;
-word = "Learning Typescript is different than Javascript";
+var word = "Learning Typescript is different than Javascript";
 var getStr = function (word, start, end) {
     return word.slice(start, end);
 };
-console.log(getStr(word, 9, 19));
-var jenis_kelamin;
-(function (jenis_kelamin) {
-    jenis_kelamin["lanang"] = "pria";
-    jenis_kelamin["hoho"] = "wanita";
-})(jenis_kelamin || (jenis_kelamin = {}));
-console.log(jenis_kelamin.lanang);
-var coba = ['aa', true, 1];
-var tupz;
+var start = 9;
+var end = 19;
+console.log(getStr(word, start, end));
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
-let word:string;
-word = "Learning Typescript is different than Javascript";
+let word:string = "Learning Typescript is different than Javascript";
 
-let getStr = function(word:string, start:number, end:number):string{
+let getStr = function(word, start, end):string{
     return word.slice(start, end)
 } 
 
-console.log(getStr(word,9, 19))
+let start:number = 9
+let end:number = 19
 
-enum jenis_kelamin {"lanang"="pria", "hoho"="wanita"}
-
-
-console.log(jenis_kelamin.lanang)
-
-let coba:(string|number|boolean)[] =['aa' , true, 1]
-
-let tupz:[number, boolean, number];
-
-
-
+console.log(getStr(word,start, end))
