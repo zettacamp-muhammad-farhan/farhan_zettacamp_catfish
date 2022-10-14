6abbd5b62b7cf7620af9757e90671bd069114a6f
muhammad.ananto <muhammad.ananto@students.amikom.ac.id>
2022-10-14 14:34:11 +0700

    day 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 var word;
-word = "indonesia merdeka";
+word = "Learning Typescript is different than Javascript";
 var getStr = function (word, start, end) {
     return word.slice(start, end);
 };
-console.log(getStr(word, 1, 9));
+console.log(getStr(word, 9, 19));
+var jenis_kelamin;
+(function (jenis_kelamin) {
+    jenis_kelamin["lanang"] = "pria";
+    jenis_kelamin["hoho"] = "wanita";
+})(jenis_kelamin || (jenis_kelamin = {}));
+console.log(jenis_kelamin.lanang);
+var coba = ['aa', true, 1];
+var tupz;
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ let getStr = function(word:string, start:number, end:number):string{
 
 console.log(getStr(word,9, 19))
 
+enum jenis_kelamin {"lanang"="pria", "hoho"="wanita"}
+
+
+console.log(jenis_kelamin.lanang)
+
+let coba:(string|number|boolean)[] =['aa' , true, 1]
+
+let tupz:[number, boolean, number];
 
 
 
