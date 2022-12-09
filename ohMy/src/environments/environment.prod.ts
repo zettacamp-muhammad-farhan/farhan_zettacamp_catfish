4fd058765268b99d27fa6dec08677f059c6e3b3f
muhammad.ananto <muhammad.ananto@students.amikom.ac.id>
2022-12-09 08:15:15 +0700

    lets

--- ohMy/src/environments/environment.prod.ts
+++ ohMy/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'api.team-8.zettacamp-upn.zetta-demo.space/graphql',
+  // apiUrl: 'https://api.team-8.zettacamp-upn.zetta-demo.space/graphql',
+  apiUrl: 'https://api.team-8.zettacamp-upn.zetta-demo.space/graphql',
   tokenKey:
     'token',
 };
